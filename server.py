5202de09695de955ab9fb5b49d8323c92bb8450f
Stroumpfgrognon <nicogalaxy78@gmail.com>
2025-09-29 22:48:27 +0200

    Interface functionnal

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 import webbrowser
 from flask import Flask, render_template, request, redirect, make_response,send_from_directory
+from crewMissionRandomizer import generate_missions
+import json
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def index():
     return send_from_directory("static","./Interface.html")
+
+@app.route('/generate',methods=['GET'])
+def generate():
+    players = request.args.get('players', default=4, type=int)
+    difficulty = request.args.get('difficulty', default=50, type=int)
+    converted = request.args.get('converted', default='false', type=str)
+    converted = True if converted.lower() == 'true' else False
+
+    # Logique de génération des missions (exemple simple)
+    missions = generate_missions(players, difficulty)
+    missions_dumpable = [{"difficulty": mission.getLevel(players), "description": mission.getDescription(converted)} for mission in missions]
+    return json.dumps(missions_dumpable)
 
 if __name__ == '__main__':
     webbrowser.open("http://localhost:8000")
